01828fe0ea53d4d7a39e14fc14ffdd817e07379f
vitor-vfm <vitor.vfm@gmail.com>
2017-03-09 20:04:43 -0700

    Implement merge sort

--- parallelmergesort.rb
+++ parallelmergesort.rb
@@ -1,94 +1,64 @@
-require 'pry'
 
 module ParallelMergeSort
 
-    # TODO: remove prints
+    # A parallel implementation of mergesort based on
+    # Cormen et al., Introduction to Algorithms, 3rd ed.,
+    # MIT Press, Cambridge, MA, USA
 
-    def self.find_correct_index(arr, value)
-        # find j such that B[j] <= value <= B[j + 1] using binary search
-        return 0 if arr.empty?
+    def self.binary_search(value, arr, i_start, i_end)
+        # find the largest index q such that arr[q-1] < value
 
-        i = 0
-        j = arr.length-1
-        while ((j-i) > 1)
-            mid = (i + j) / 2
-            if (arr[mid] <= value and value <= arr[mid+1])
-                return mid
-            end
+        low = i_start
+        high = [i_start, i_end+1].max
 
-            if (value <= arr[mid])
-                j = mid
-            elsif (value >= arr[mid+1])
-                i = mid
+        while (low < high)
+            mid = (low + high) / 2
+            if value <= arr[mid]
+                high = mid
+            else
+                low = mid + 1
             end
         end
 
-        if (j == arr.length-1)
-            return j
-        else
-            return i
-        end
+        high
     end
             
 
-    def self.pmerge(a, b, col, i_start, i_end)
-        # merge the arrays a and b into col[i_start..i_end]
+    def self.pmerge(from, p1, r1, p2, r2, to, p3)
+        # merge in parallel the arrays from[p1..r1] and from[p2..r2] 
+        # into the array slice starting at to[p3]
 
-        # dummy version - not parallel
+        size1 = r1 - p1 + 1
+        size2 = r2 - p2 + 1
+        if size1 < size2 # the first array should be the largest
+            self.pmerge(from, p2, r2, p1, r1, to, p3)
+        end
 
-        puts "merging #{a} and #{b} into indices #{i_start}, #{i_end}"
-        puts "col: #{col}"
-        output_size = i_end - i_start + 1
-
-        # without loss of generality, larger array should be first
-        return pmerge(b, a, col, i_start, i_end) if b.length > a.length
-
-        if output_size == 1
-            col[i_start] = a[0]
-        elsif a.empty?
+        if size1 == 0 # both empty
             return
-        elsif a.length == 1
-            if a[0] <= b[0]
-                col[i_start] = a[0]
-                col[i_start+1] = b[0]
-            else
-                col[i_start] = b[0]
-                col[i_start+1] = a[0]
-            end
         else
-            pivot_index = a.length/2
-            pivot = a[pivot_index]
+            mid_slice1 = (p1 + r1) / 2
+            index_in_slice2 = self.binary_search(from[mid_slice1], from, p2, r2)
+            division_index = p3 + (mid_slice1 - p1) + (index_in_slice2 - p2)
+            to[division_index] = from[mid_slice1]
 
-            j = find_correct_index(b, pivot) # index that pivot should have in b
-            puts "index of value #{pivot} in #{b} is #{j}"
-
-            a_slice1 = a[0..(pivot_index-1)]
-            a_slice2 = a[(pivot_index + 1)..(-1)]
-
-            b_slice1 = b[0..j]
-            b_slice2 = b[(j+1)..(-1)]
-
-            col_slice1_end = i_start + a_slice1.length + j
-            col_slice2_start = col_slice1_end + 2
-            col[col_slice1_end + 1] = pivot
-
-            pmerge(a_slice1, b_slice1, col, i_start, col_slice1_end)
-            pmerge(a_slice2, b_slice2, col, col_slice2_start, i_end)
+            t1 = Thread.new { self.pmerge(from, p1, mid_slice1 - 1, p2, index_in_slice2 - 1, to, p3) }
+            t2 = Thread.new { self.pmerge(from, mid_slice1 + 1, r1, index_in_slice2, r2, to, division_index + 1) }
+            t1.join
+            t2.join
         end
     end
 
     def self.merge(collection, p, q, r)
-        first_half_copy = collection[p..q]
-        second_half_copy = collection[q+1..r]
-        collection.fill(0) # TODO: remove = only for debugging
-        pmerge(first_half_copy, second_half_copy, collection, p, r)
+        backup = collection[p..r]
+        self.pmerge(backup, 0, q - p, q - p + 1, r - p, collection, p)
     end
 
     def self.mergesort(collection, p, r)
         if p < r
             q = (p + r) / 2
-            t1 = Thread.new { mergesort(collection, p, q) }
-            t1 = Thread.new { mergesort(collection, q+1, r) }
+            t1 = Thread.new { self.mergesort(collection, p, q) }
+            t2 = Thread.new { self.mergesort(collection, q+1, r) }
             t1.join()
             t2.join()
             merge(collection, p, q, r)
